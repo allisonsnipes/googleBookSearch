296347a47ca3c0820b300d97b579a0574c596b47
Allison Snipes <ajsnipes@usfca.edu>
2019-08-05 15:35:38 -0700

    setting up

---
+++ 07-Ins_Mern/create-react-express/client/build/precache-manifest.bf1aadae084fd1eed5e53bfd0c272285.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e4df21f306c71348ad39",
+    "url": "/static/css/main.e617aa2a.chunk.css"
+  },
+  {
+    "revision": "e4df21f306c71348ad39",
+    "url": "/static/js/main.a47dc933.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "495d93cc2c05774c4e88882137125113",
+    "url": "/static/media/logo.495d93cc.svg"
+  },
+  {
+    "revision": "976d9c3722a52a8ed2dd840f526af0ce",
+    "url": "/index.html"
+  }
+];
